02c39639837f7dd10bc1c3fbc8064da26213f72e
hejiajun <hejiajun@bonc.com.cn>
2018-09-13 15:31:38 +0800

    允许跨域

--- src/main/java/com/bonc/medicine/MedicineApplication.java
+++ src/main/java/com/bonc/medicine/MedicineApplication.java
@@ -3,12 +3,23 @@ package com.bonc.medicine;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication(scanBasePackages = "com.bonc.medicine")
 @MapperScan("com.bonc.medicine.mapper")
-public class MedicineApplication {
+public class MedicineApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(MedicineApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
