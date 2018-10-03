55c59a3df2c121176ec5e5392b1b06daf0c0573b
陈佳俊 <chenjiajun@bonc.com.cn>
2018-10-03 12:06:54 +0800

    接口优化

--- src/main/java/com/bonc/medicine/controller/knowledgebase/SopController.java
+++ src/main/java/com/bonc/medicine/controller/knowledgebase/SopController.java
@@ -1,5 +1,6 @@
 package com.bonc.medicine.controller.knowledgebase;
 
+import com.bonc.medicine.annotation.CurrentUser;
 import com.bonc.medicine.entity.Result;
 import com.bonc.medicine.service.knowledgebase.AuditService;
 import com.bonc.medicine.service.knowledgebase.SopService;
@@ -40,7 +41,7 @@ public class SopController {
     @SuppressWarnings("unchecked")
     @PostMapping("/addPlantSopStep")
     @Transactional
-    public Result<Object> addSops(@RequestBody String addJson){
+    public Result<Object> addSops(@RequestBody String addJson, @CurrentUser String userId) {
          //addJson传入两部分，一部分为sop基本信息，一部分为sop步骤信息
         Map map = JacksonMapper.INSTANCE.readJsonToMap(addJson);
         Map sopMap = (Map) map.get("sop");//获取sop基本信息
@@ -48,10 +49,12 @@ public class SopController {
         sopMap.put("sop_status", 1);
         //审核状态
         sopMap.put("record_status", 2);
+        //录入人id
+        System.out.println("userId:" + userId);
+        sopMap.put("record_user_id", userId);
         int count = sopService.sopAdd(sopMap);
         //从mybatis获取自增主键
         Long id = (Long) sopMap.get("id");
-        System.out.println("*******" + id);
         //获取sop步骤信息
         List sopStepList = (List) map.get("sopStep");
         for (int i = 0; i < sopStepList.size(); i++) {
@@ -63,7 +66,7 @@ public class SopController {
         Map auditMap = new HashMap();
         auditMap.put("km_type","4");
         auditMap.put("id", id);
-        String chinese_name = (String) sopMap.get("chinese_name");
+        String chinese_name = (String) sopMap.get("name");
         auditMap.put("title", chinese_name);
         count += auditService.addAudit(auditMap);
         count += sopService.sopStepAdd(sopStepList);
@@ -73,10 +76,13 @@ public class SopController {
     @SuppressWarnings("unchecked")
     @PutMapping("/editSop")
     @Transactional
-    public Result<Object> editSops(@RequestBody String editJson){
+    public Result<Object> editSops(@RequestBody String editJson, @CurrentUser String userId) {
         Map map = JacksonMapper.INSTANCE.readJsonToMap((editJson));
         Map sopMap = (Map) map.get("sop");//获取sop基本信息
         int id = (int) sopMap.get("id");//获取id
+        //录入人id
+        System.out.println("userId:" + userId);
+        sopMap.put("record_user_id", userId);
         //删除当前所有步骤，再重新添加所有信息到sop步骤
         tombstoneStep(id);
         List sopStepList = (List) map.get("sopStep");//获取sop步骤信息
@@ -89,6 +95,7 @@ public class SopController {
         //更改审核表
         Map auditMap = new HashMap();
         String chinese_name = (String) sopMap.get("name");
+        System.out.println(chinese_name);
         auditMap.put("title", chinese_name);
         auditMap.put("km_type","4");
         auditMap.put("id", id);
