b6be5860a58b385c58e292d9b7966296194855d2
hejiajun <hejiajun@bonc.com.cn>
2018-10-03 16:24:36 +0800

    新增后台管理视频/直播/线下培训的接口

--- src/main/java/com/bonc/medicine/service/thumb/ViewNumberService.java
+++ src/main/java/com/bonc/medicine/service/thumb/ViewNumberService.java
@@ -1,5 +1,6 @@
 package com.bonc.medicine.service.thumb;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,6 +23,19 @@ public interface ViewNumberService {
     * @Date: 2018/9/30
     */
     public Map<String, Object> addOrUpdateViewNumberCord(Map<String, String> map);
+
+    /**
+    * @Description:获取直播、视频、线下培训 什么数什么数等的统计接口
+    * @Param: [videoIds:必须--可以传入多个 多个用“，”分开,
+     *         videoType ：必须--1 线下培训  2 直播  0：视频 ]
+    * @return: java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
+     *     返回 预约数；观看数；互动数；点赞数；
+     *
+     *     如果参数不全就返回null；如果在调用该接口的时候先判断返回值是不是null
+    * @Author: hejiajun
+    * @Date: 2018/10/1
+    */
+    public List<Map<String, Object>> videoDetailNumberStatistical(String videoIds, String videoType);
 
 
 }
