4687e1a1b36bc125a43970781a555a18d99778ed
tanglizhong <tanglizhonrg@bonc.com.cn>
2018-10-08 10:04:44 +0800

    线下培训视频修改   数据库增加  video_title字段

--- src/main/java/com/bonc/medicine/mapper/information/TrainMapper.java
+++ src/main/java/com/bonc/medicine/mapper/information/TrainMapper.java
@@ -41,7 +41,7 @@ public interface TrainMapper {
     List<Map> selectTrainList(Map<String, Object> map);
 
 
-    @Insert("insert into train_offline_video(train_id,video_url,upload_user_id) values (#{train_id},#{video_url},#{user_id}) ")
+    @Insert("insert into train_offline_video(train_id,video_title,video_url,upload_user_id) values (#{train_id},#{video_title},#{video_url},#{user_id}) ")
     int editOfflineTrainVideo(Map<String, Object> map);
 
 
