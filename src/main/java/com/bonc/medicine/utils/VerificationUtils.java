82efa96948e8e8c15486e1e5215d92a7e4e45ce3
hejiajun <hejiajun@bonc.com.cn>
2018-10-18 18:00:21 +0800

    修改验证码可能出现的bug

--- src/main/java/com/bonc/medicine/utils/VerificationUtils.java
+++ src/main/java/com/bonc/medicine/utils/VerificationUtils.java
@@ -1,5 +1,7 @@
 package com.bonc.medicine.utils;
 
+import java.util.Random;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.alibaba.fastjson.JSON;
@@ -18,82 +20,89 @@ import com.bonc.medicine.enums.ResultEnum;
  **/
 public class VerificationUtils {
 
-    /**
-    * @Description: 通过手机号码获取验证码
-    * @Param: [phone] 手机号码
-    * @return: java.lang.String
-    * @Author: hejiajun
-    * @Date: 2018/9/3
-    */
-    public static String getVerifByPhone(String phone){
+	/**
+	 * @Description: 通过手机号码获取验证码
+	 * @Param: [phone] 手机号码
+	 * @return: java.lang.String
+	 * @Author: hejiajun
+	 * @Date: 2018/9/3
+	 */
+	public static String getVerifByPhone(String phone) {
 
-        // TODO 可能这个正则需要改动
-        String pattern = "^((1[3,5,8][0-9])|(14[5,7])|(17[0,6,7,8])|(19[9]))\\d{8}$";
+		// TODO 可能这个正则需要改动
+		String pattern = "^((1[3,5,8][0-9])|(14[5,7])|(17[0,6,7,8])|(19[9]))\\d{8}$";
 
-        if (StringUtils.isEmpty(phone) || !phone.matches(pattern)){
-            throw new MedicineRuntimeException(ResultEnum.MISSING_PARA);
-        }
-        int code = 0;
-        try{
-            code  = methodOfGeneratingGourDigits();
+		if (StringUtils.isEmpty(phone) || !phone.matches(pattern)) {
+			throw new MedicineRuntimeException(ResultEnum.MISSING_PARA);
+		}
+		int code = 0;
+		try {
+			code = methodOfGeneratingGourDigits();
 
-            String realCode = HttpTool.sendPost(phone, code);
+			String realCode = HttpTool.sendPost(phone, code);
 
-            JSONObject json = JSON.parseObject(realCode);
-            String  status = json.getString("respstatus");
-            if (!StringUtils.equals("0", status)){
-                throw new MedicineRuntimeException(ResultEnum.NET_ERROR);
-            }
+			JSONObject json = JSON.parseObject(realCode);
+			String status = json.getString("respstatus");
+			if (!StringUtils.equals("0", status)) {
+				throw new MedicineRuntimeException(ResultEnum.NET_ERROR);
+			}
 
-        }catch (Exception e){
+		} catch (Exception e) {
 
-            throw new MedicineRuntimeException(ResultEnum.NET_ERROR);
-        }
+			throw new MedicineRuntimeException(ResultEnum.NET_ERROR);
+		}
 
-        // TODO zheli 需要进一步实现获取验证码，如果是用后端接口
+		// TODO zheli 需要进一步实现获取验证码，如果是用后端接口
 
-        return code + "";
-    }
+		return code + "";
+	}
 
-    /**
-    * @Description: 验证验证码是否正确，
-    * @Param: [verification：验证码, phone：手机号码]
-    * @return: boolean
-    * @Author: hejiajun
-    * @Date: 2018/9/3
-    */
- /*   public static boolean validateVerification(String verification, String phone) {
+	/**
+	 * @Description: 验证验证码是否正确，
+	 * @Param: [verification：验证码, phone：手机号码]
+	 * @return: boolean
+	 * @Author: hejiajun
+	 * @Date: 2018/9/3
+	 */
+	/*
+	 * public static boolean validateVerification(String verification, String
+	 * phone) {
+	 * 
+	 * if (StringUtils.isEmpty(verification) ||
+	 * !StringUtils.equals(verification.trim(), getVerifByPhone(phone))) {
+	 * return false; }
+	 * 
+	 * return true; }
+	 */
 
-        if (StringUtils.isEmpty(verification)
-                || !StringUtils.equals(verification.trim(), getVerifByPhone(phone))) {
-            return false;
-        }
+	/**
+	 * @Description: 生成一个四位数的验证码
+	 * @Param: []
+	 * @return: int
+	 * @Author: hejiajun
+	 * @Date: 2018/9/20
+	 */
+	private static int methodOfGeneratingGourDigits() {
+		String str = "0123456789";
+		StringBuilder sb = new StringBuilder(4);
+		for (int i = 0; i < 4; i++) {
+			char ch = str.charAt(new Random().nextInt(str.length()));
+			sb.append(ch);
+		}
+		return Integer.parseInt(sb.toString());
+	}
 
-        return true;
-    }*/
+/*	public static void main(String[] args) {
+		String str = "0123456789";
+		StringBuilder sb = new StringBuilder(4);
+		for (int i = 0; i < 4; i++) {
+			char ch = str.charAt(new Random().nextInt(str.length()));
+			sb.append(ch);
+		}
+			if (sb.toString().length() != 4) {
+				System.out.println(sb.toString());
+			}
 
-    /**
-    * @Description: 生成一个四位数的验证码
-    * @Param: []
-    * @return: int
-    * @Author: hejiajun
-    * @Date: 2018/9/20 
-    */ 
-    private static int methodOfGeneratingGourDigits(){
-        double codeDemo = Math.random();
-        String code = String.valueOf(codeDemo).substring(2, 6);
-        return Integer.parseInt(code);
-    }
-
- /*  public static void main(String[] args) {
-       for (int i = 0; i < 10000000; i++){
-           double codeDemo = Math.random();
-           String code = String.valueOf(codeDemo).substring(2, 6);
-           if (code.length() != 4){
-               System.out.println(code);
-           }
-
-       }
-    }*/
+	}*/
 
 }
